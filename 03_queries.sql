747c6ffb17c15020ff5a10aeec0f8fed15b1c652
swings27 <anna.trbd@gmail.com>
2026-03-26 16:56:47 +0100

    query 1 done

---
+++ 03_queries.sql
@@ -0,0 +1,8 @@
+-- Requête 1 : Afficher la liste des noms des focaccias par ordre alphabétique croissant
+-- Résultat attendu : Une liste avec les noms de toutes les focaccias rangés par ordre alphabétique croissant, sans les id et prix
+-- Code :
+SELECT nom 
+FROM focaccia 
+ORDER BY nom ASC;
+-- Résultat obtenu : Liste alphabétique des noms des focaccias débutant par Américaine et se terminant par Tradizione
+
