de8adb8c78058c4c265fba06c5f6696c118f9f59
swings27 <anna.trbd@gmail.com>
2026-03-26 17:48:03 +0100

    query 4 and 5 checked

--- 03_queries.sql
+++ 03_queries.sql
@@ -21,3 +21,23 @@
     AS AveragePrice
     FROM focaccia;
 -- Résultat obtenu : Le nombre 10.375000 dans la colonne AveragePrice
+
+-- Requête 4 : Afficher la liste des boissons avec leur marque, triée par nom de boisson
+-- Résultat attendu : La liste alphabétique des boissons avec 2 colonnes pour leur nom et la marque associée, sans id
+-- Code :
+    SELECT boisson.nom, marque.nom AS nom_marque
+    FROM boisson 
+    INNER JOIN marque ON boisson.id_marque = marque.id_marque
+    ORDER BY boisson.nom ASC;
+-- Résultat obtenu : Une liste des boissons et de leurs marques associées
+
+-- Requête 5 : Afficher la liste des ingrédients pour une Raclaccia
+-- Résultat attendu : La liste des ingrédients de la Raclaccia avec leurs noms
+-- Code :
+    SELECT ingredient.nom
+    FROM ingredient
+    JOIN comprend ON ingredient.id_ingredient = comprend.id_ingredient
+    JOIN focaccia ON focaccia.id_focaccia = comprend.id_focaccia
+    WHERE focaccia.nom = 'Raclaccia'
+    ORDER BY ingredient.nom ASC;
+-- Résultat obtenu : La liste des noms des ingrédients compris dans la Raclaccia
