0b298ee516bad4aac9751d8f9eaa4e490d92cdf0
swings27 <anna.trbd@gmail.com>
2026-03-27 11:42:47 +0100

    Query 6, 7 and 8 checked

--- 03_queries.sql
+++ 03_queries.sql
@@ -41,3 +41,45 @@
     WHERE focaccia.nom = 'Raclaccia'
     ORDER BY ingredient.nom ASC;
 -- Résultat obtenu : La liste des noms des ingrédients compris dans la Raclaccia
+
+-- Requête 6 : Afficher le nom et le nombre d'ingrédients pour chaque foccacia
+-- Résultat attendu : 2 colonnes, l'un avec le nom de la focaccia et l'autre avec le nombre d'ingrédients correspondant
+-- Code :
+    SELECT focaccia.nom, COUNT(comprend.id_ingredient) AS nb_ingredient
+    FROM focaccia
+    JOIN comprend ON focaccia.id_focaccia = comprend.id_focaccia
+    GROUP BY focaccia.nom
+-- Résultat obtenu : 2 colonnes avec le nom de chaque focaccia par ordre alphabétique et le nombre d'ingrédients lui correspondant
+
+-- Requête 7 : Afficher le nom de la focaccia qui a le plus d'ingrédients
+-- Résultat attendu : Uniquement le nom de la focaccia avec le plus d'ingrédients, sans préciser le nombre
+-- Code :
+    SELECT focaccia.nom
+    FROM focaccia
+    JOIN comprend ON focaccia.id_focaccia = comprend.id_focaccia
+    GROUP BY focaccia.nom
+    ORDER BY COUNT(comprend.id_ingredient) 
+    DESC LIMIT 1;
+-- Résultat obtenu : Le nom de la focaccia "Paysanne" qui compte le plus d'ingrédients sans afficher le nombre
+
+-- Requête 8 : Afficher la liste des focaccia qui contiennent de l'ail
+-- Résultat attendu : Une liste en colonne unique des focaccias avec de l'ail
+-- Code :
+    SELECT focaccia.nom
+    FROM focaccia
+    JOIN comprend ON focaccia.id_focaccia = comprend.id_focaccia
+    JOIN ingredient ON ingredient.id_ingredient = comprend.id_ingredient
+    WHERE comprend.id_ingredient = 1
+-- Résultat obtenu : La liste alphabétique des focaccias contenant de l'ail (Mozaccia, Gorgonzollaccia, Raclaccia et Paysanne)
+
+-- Requête 9 : Afficher la liste des ingrédients inutilisés
+-- Résultat attendu : 
+-- Code :
+    SELECT 
+-- Résultat obtenu : La liste des noms des ingrédients compris dans la Raclaccia
+
+-- Requête 10 : Afficher la liste des focaccia qui n'ont pas de champignons
+-- Résultat attendu : 
+-- Code :
+    SELECT 
+-- Résultat obtenu :
